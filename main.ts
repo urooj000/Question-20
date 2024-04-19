087ae8d1263b485461f1a6f09a4d6108eeecb88a
urooj22 <160038688+urooj000@users.noreply.github.com>
2024-04-19 22:08:06 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,18 @@
+// Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items.
+
+let mountains: string[] = [
+      "Mount Everest",
+      "K2",
+     "Cho Oyo",
+      "Kangchenjunga",
+       "Lhotse", 
+       "Makalu", 
+       "Dhaulagiri",
+       "Manaslu",
+       "Nanga parbat",
+       "Annapurna"
+];
+console.log ("List of famous mountains:");
+mountains.forEach((mountain, index) =>  {
+    console.log(`${index + 1}. ${mountain}`);
+});
